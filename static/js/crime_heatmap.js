80ab60b3a8765c1ae7109a61a849aa06074ede3f
Erica Davis <ericawdavis@ericas-mbp-3.dhcp.wustl.edu>
2019-02-16 12:32:30 -0600

    added htmlupdates

--- static/js/crime_heatmap.js
+++ static/js/crime_heatmap.js
@@ -1,10 +1,9 @@
-
 //need "crime_heatmap tag in html"
 // var myMap = L.map("map", {
 //     center: [41.8781, 87.6298],
 //     zoom: 13
 //   });
-  
+
 //   L.tileLayer("https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token={accessToken}", {
 //     attribution: "Map data &copy; <a href='https://www.openstreetmap.org/'>OpenStreetMap</a> contributors, <a href='https://creativecommons.org/licenses/by-sa/2.0/'>CC-BY-SA</a>, Imagery © <a href='https://www.mapbox.com/'>Mapbox</a>",
 //     maxZoom: 18,
@@ -12,26 +11,25 @@
 //     accessToken: MAPBOX_API_KEY
 //   }).addTo(myMap);
 
+//get the data
+url = "/crime_data/";
 
-  //get the data
-  url = "/crime_data/"
+d3.json(url, function(response) {
+  // console.log(response)
+  var heatArray = [];
 
-  d3.json(url, function(response){
-      console.log(response)
-      var heatArray = [];
+  for (var i = 0; i < response.length; i++) {
+    var coordinates = [response[i].latitude, response[i].longitude];
 
-      for (var i = 0; i < response.length; i++) {
-        var coordinates = [response[i].latitude, response[i].longitude]
+    // console.log(coordinates)
 
-        console.log(coordinates)
-    
-        if (location) {
-          heatArray.push(coordinates);
-        }
-      }
-    
-      var heat = L.heatLayer(heatArray, {
-        radius: 20,
-        blur: 35
-      }).addTo(myMap);
-  });
+    if (location) {
+      heatArray.push(coordinates);
+    }
+  }
+
+  var heat = L.heatLayer(heatArray, {
+    radius: 20,
+    blur: 35
+  }).addTo(myMap);
+});
