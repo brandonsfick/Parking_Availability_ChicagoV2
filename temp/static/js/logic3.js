bd4336ea7cc166024d841159311cf7e8dbbbecfe
rayanh13 <43301872+rayanh13@users.noreply.github.com>
2019-02-18 08:22:01 -0600

    Update logic3.js

--- temp/static/js/logic3.js
+++ temp/static/js/logic3.js
@@ -18,17 +18,17 @@ function onMapClick(e) {
      // Marker Icon
 var redIcon = L.icon({
     iconUrl: "marker.png",
-    iconSize: [25, 50] // size of the icon
+    iconSize: [25, 40] // size of the icon
   });
   
   var yellowIcon = L.icon({
     iconUrl: "marker4.png",
-    iconSize: [25, 50] // size of the icon
+    iconSize: [25, 40] // size of the icon
   });
   
   var blackIcon = L.icon({
     iconUrl: "marker3.png",
-    iconSize: [25, 50] // size of the icon
+    iconSize: [25, 40] // size of the icon
   });
     // Grab the data with d3
     d3.json(url2, function(response) {
